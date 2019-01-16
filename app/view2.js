bac5a354296cf69cb728a546611869c56e8b19bd
Vivian Zaddach <vivian.zaddach@idealo.de>
2019-01-16 15:14:57 +0100

    renaming and formatting

--- app/view2.js
+++ app/view2.js
@@ -11,24 +11,23 @@ app.controller("HelloController", function ($scope, $http) {
         $scope.ergebnisse = [];
 
         $http.get("http://localhost:5000/getAllDistrictsOf?id=" + value)
-            .then(function(response) {
+            .then(function (response) {
                 $scope.wahlkreise = JSON.parse(response.data);
             });
     };
 
-    $scope.showDiagram = function (value) {
-
-    $http.get("http://localhost:5000/getVotesOf?id=" + 1)
-        .then(function(response) {
-        console.log(response.data);
-            $scope.ergebnisse = JSON.parse(response.data);
-        });
+    $scope.showPartyTable = function (value) {
+        $http.get("http://localhost:5000/getVotesOf?id=" + value)
+            .then(function (response) {
+                console.log(response.data);
+                $scope.ergebnisse = JSON.parse(response.data);
+            });
     };
 
     $scope.init = function () {
-        $http.get('http://localhost:5000/getAllStates').then(function(response){
-            // the success -case
-            $scope.bundeslaender = JSON.parse(response.data); }, function(response){
+        $http.get('http://localhost:5000/getAllStates').then(function (response) {
+            $scope.bundeslaender = JSON.parse(response.data);
+        }, function (response) {
             // the error -case
             console.log(response);
         });
